b25b4e45b513594e537d364bbf9eccfa69b00851
Michael Ghen <mike@mikeghen.com>
2021-12-09 13:32:42 -0500

    swap operator and workflow

---
+++ dags/blocksec_plugin/uniswap_swap_exact_tokens_for_eth_operator.py
@@ -0,0 +1,92 @@
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from blocksec_plugin.web3_hook import Web3Hook
+from blocksec_plugin.ethereum_wallet_hook import EthereumWalletHook
+import requests,json
+from time import sleep
+
+SWAP_ABI = '''[{
+    "inputs":[
+        {"internalType":"uint256","name":"amountIn","type":"uint256"},
+        {"internalType":"uint256","name":"amountOutMin","type":"uint256"},
+        {"internalType":"address[]","name":"path","type":"address[]"},
+        {"internalType":"address","name":"to","type":"address"},
+        {"internalType":"uint256","name":"deadline","type":"uint256"}
+    ],
+    "name":"swapExactTokensForETH",
+    "outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],
+    "stateMutability":"nonpayable","type":"function"
+}]'''
+
+class UniswapSwapExactTokensForETHOperator(BaseOperator):
+    """
+    Calls `swapExactTokensForETH` on a Uniswap Router contract
+    * Assumes amount of tokens has been approved already
+    """
+    template_fields = []
+
+    @apply_defaults
+    def __init__(self,
+                 web3_conn_id='web3_default',
+                 ethereum_wallet='default_wallet',
+                 router_address,
+                 amount_in=0,
+                 amount_out_min=0,
+                 path=[],
+                 to=None,
+                 deadline=None,
+                 gas_key="fast",
+                 gas_multiplier=1,
+                 gas=1200000,
+                 nonce=None,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.router_address = router_address
+        self.input_token = input_token
+        self.output_token = output_token
+        self.amount_in = amount_in
+        self.amount_out_min = amount_out_min
+        self.path = path
+        self.to = to
+        if not deadline:
+            self.deadline = now + 300
+        else:
+            self.deadline = dealine
+        self.web3_conn_id = web3_conn_id
+        self.ethereum_wallet = ethereum_wallet
+        self.contract_address = router_address
+        self.abi_json = SWAP_ABI
+        self.gas_key = gas_key
+        self.gas_multiplier = gas_multiplier
+        self.gas = gas
+        self.web3 = Web3Hook(web3_conn_id=self.web3_conn_id).http_client
+        self.wallet = EthereumWalletHook(ethereum_wallet=self.ethereum_wallet)
+        if nonce:
+            self.nonce = nonce
+        else: # Look up the last nonce for this wallet
+            self.nonce = self.web3.eth.getTransactionCount(self.wallet.public_address)
+
+    def execute(self, context):
+        # Create the contract factory
+        print("Processing distribution for Ricochet at {0} by EOA {1}".format(
+            self.contract_address, self.wallet.public_address
+        ))
+        contract = self.web3.eth.contract(self.contract_address, abi=self.abi_json)
+        # Form the signed transaction
+        withdraw_txn = contract.functions.swapExactTokensForETH(self.amount_in,
+                                                                self.amount_out_min,
+                                                                self.path,
+                                                                self.to,
+                                                                self.deadline)\
+                                         .buildTransaction(dict(
+                                           nonce=int(self.nonce),
+                                           gasPrice = int(self.web3.eth.gasPrice *\
+                                                      self.gas_multiplier),
+                                           gas = self.gas
+                                          ))
+        signed_txn = self.web3.eth.account.signTransaction(withdraw_txn, self.wallet.private_key)
+        # Send the transaction
+        transaction_hash = self.web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+        print(f"Sent swapExactTokensForETH({self.amount_in},{self.amount_out_min},{self.path},{self.to},{self.deadline})... transaction hash: {transaction_hash.hex()}")
+        return str(transaction_hash.hex()) # Return for use with EthereumTransactionConfirmationSensor
---
+++ dags/swap_example.py
@@ -0,0 +1,95 @@
+"""
+
+Example Swap Operation
+- Swap RIC for MATIC
+
+"""
+from airflow import DAG
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from blocksec_plugin.web3_hook import Web3Hook
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from blocksec_plugin.ethereum_transaction_confirmation_sensor import EthereumTransactionConfirmationSensor
+from blocksec_plugin.tellor_oracle_operator import TellorOracleOperator
+from blocksec_plugin.erc20_approve_operator import ERC20ApprovalOperator
+from blocksec_plugin.uniswap_swap_exact_tokens_for_eth_operator import UniswapSwapExactTokensForETHOperator
+from blocksec_plugin.abis import TELLOR_ABI
+from json import loads
+import requests
+
+SWAPPER_WALLET_ADDRESS = Variable.get("distributor-address")
+
+default_args = {
+    "owner": "ricochet",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 3, 29),
+    "email": ["mike@mikeghen.com"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1)
+}
+
+
+dag = DAG("swap_example",
+          max_active_runs=1,
+          catchup=False,
+          default_args=default_args,
+          schedule_interval="0 * * * *")
+
+web3 = Web3Hook(web3_conn_id='infura').http_client
+current_nonce = web3.eth.getTransactionCount(SWAPPER_WALLET_ADDRESS)
+
+done = BashOperator(
+    task_id='done',
+    bash_command='date',
+    dag=dag,
+)
+
+approve = ERC20ApprovalOperator(
+    task_id="approve",
+    web3_conn_id="infura",
+    ethereum_wallet=SWAPPER_WALLET_ADDRESS,
+    gas_multiplier=1.2,
+    gas=3000000,
+    contract_address="0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+    spender="0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+    amount=1000000000000000000
+    dag=dag
+)
+
+confirm_approve = EthereumTransactionConfirmationSensor(
+    task_id="confirm_approve",
+    web3_conn_id="infura",
+    transaction_hash="{{task_instance.xcom_pull(task_ids='approve')}}",
+    confirmations=1,
+    poke_interval=5,
+    timeout=60 * 20,
+    dag=dag
+)
+
+swap = UniswapSwapExactTokensForETHOperator(
+    task_id="swap",
+    web3_conn_id="infura",
+    ethereum_wallet=SWAPPER_WALLET_ADDRESS,
+    gas_multiplier=1.2,
+    gas=3000000,
+    router_address="0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+    amount_in=10000000000000000000,
+    amount_out_min=0,
+    path=["0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174","0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"]
+    dag=dag
+)
+
+confirm_swap = EthereumTransactionConfirmationSensor(
+    task_id="confirm_swap",
+    web3_conn_id="infura",
+    transaction_hash="{{task_instance.xcom_pull(task_ids='swap')}}",
+    confirmations=1,
+    poke_interval=5,
+    timeout=60 * 20,
+    dag=dag
+)
+
+    done << confirm_swap << swap
