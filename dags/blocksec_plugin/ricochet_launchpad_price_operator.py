bbfa5018483015e5132d5a515b92ab85441d1914
Michael Ghen <mike@mikeghen.com>
2021-12-10 14:45:42 -0500

    check launchpad price operater

---
+++ dags/blocksec_plugin/ricochet_launchpad_price_operator.py
@@ -0,0 +1,43 @@
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from blocksec_plugin.web3_hook import Web3Hook
+from blocksec_plugin.ethereum_wallet_hook import EthereumWalletHook
+
+SHARE_PRICE_ABI = '''[{"inputs":[],"name":"getSharePrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'''
+
+class RicochetLaunchpadPrice(BaseOperator):
+    """
+    Checks the price of a Ricochet Launchpad
+    """
+    template_fields = []
+
+    @apply_defaults
+    def __init__(self,
+                 web3_conn_id='web3_default',
+                 ethereum_wallet='default_wallet',
+                 contract_address=None,
+                 gas_key="fast",
+                 gas_multiplier=1,
+                 gas=1200000,
+                 nonce=None,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.web3_conn_id = web3_conn_id
+        self.ethereum_wallet = ethereum_wallet
+        self.contract_address = contract_address
+        self.abi_json = SHARE_PRICE_ABI
+        self.gas_key = gas_key
+        self.gas_multiplier = gas_multiplier
+        self.gas = gas
+        self.web3 = Web3Hook(web3_conn_id=self.web3_conn_id).http_client
+        self.wallet = EthereumWalletHook(ethereum_wallet=self.ethereum_wallet)
+        if nonce:
+            self.nonce = nonce
+        else: # Look up the last nonce for this wallet
+            self.nonce = self.web3.eth.getTransactionCount(self.wallet.public_address)
+
+    def execute(self, context):
+        contract = self.web3.eth.contract(self.contract_address, abi=self.abi_json)
+        share_price = contract.functions.getSharePrice().call()
+        return share_price
