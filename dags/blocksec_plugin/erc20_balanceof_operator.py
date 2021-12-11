c50764d9554f937362d891f91713ab408733b25b
Michael Ghen <mike@mikeghen.com>
2021-12-11 15:04:35 -0500

    fix typos

--- dags/blocksec_plugin/erc20_balanceof_operator.py
+++ dags/blocksec_plugin/erc20_balanceof_operator.py
@@ -1,9 +1,6 @@
 from airflow.models.baseoperator import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from blocksec_plugin.web3_hook import Web3Hook
-from blocksec_plugin.ethereum_wallet_hook import EthereumWalletHook
-import requests,json
-from time import sleep
 
 ERC20_ABI = '''
 [{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
@@ -27,9 +24,5 @@ class ERC20BalanceOfOperator(BaseOperator):
         self.web3 = Web3Hook(web3_conn_id=self.web3_conn_id).http_client
 
     def execute(self, context):
-        # Create the contract factory
-        print("Processing approve of {0} by EOA {1}".format(
-            self.contract_address, self.wallet.public_address
-        ))
         contract = self.web3.eth.contract(self.contract_address, abi=ERC20_ABI)
         return contract.functions.balanceOf(self.account).call()
