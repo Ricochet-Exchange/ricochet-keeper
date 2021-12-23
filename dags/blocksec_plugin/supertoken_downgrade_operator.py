6044758fede86cdf0c4cda1611a4454572c00f5e
Michael Ghen <mike@mikeghen.com>
2021-12-23 14:54:29 -0500

    fix typo

--- dags/blocksec_plugin/supertoken_downgrade_operator.py
+++ dags/blocksec_plugin/supertoken_downgrade_operator.py
@@ -20,7 +20,7 @@ class SuperTokenDowngradeOperator(ContractInteractionOperator):
     def execute(self, context):
         if int(self.amount) < 0:
             # Max downgrade
-            self.amount = contract.functions.balanceOf(self.wallet.public_address).call()
+            self.amount = self.contract.functions.balanceOf(self.wallet.public_address).call()
         self.function = self.contract.functions.downgrade
         self.function_args = {"amount": int(self.amount)}
         return super().execute(context)
