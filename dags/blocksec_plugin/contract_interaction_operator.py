fb99fa80129e4ccb55e189eb9827649b99a627aa
Michael Ghen <mike@mikeghen.com>
2021-12-23 13:00:06 -0500

    try reverting

--- dags/blocksec_plugin/contract_interaction_operator.py
+++ dags/blocksec_plugin/contract_interaction_operator.py
@@ -2,14 +2,12 @@ from airflow.models.baseoperator import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from blocksec_plugin.web3_hook import Web3Hook
 from blocksec_plugin.ethereum_wallet_hook import EthereumWalletHook
-from web3.exceptions import InvalidAddress
 
 class ContractInteractionOperator(BaseOperator):
     """
     Executes a generalized contract interaction
     """
-
-    template_fields = ['contract_address', 'function', 'args', 'abi_json']
+    template_fields = ['contract_address', 'args', 'abi_json']
 
     @apply_defaults
     def __init__(self,
@@ -36,24 +34,25 @@ class ContractInteractionOperator(BaseOperator):
         self.gas = gas
         self.web3 = Web3Hook(web3_conn_id=self.web3_conn_id).http_client
         self.wallet = EthereumWalletHook(ethereum_wallet=self.ethereum_wallet)
+
         try: # check if this is set, otherwise set it with
             self.abi_json
         except AttributeError:
             self.abi_json = abi_json
+
         if nonce:
             self.nonce = nonce
         else: # Look up the last nonce for this wallet
             self.nonce = self.web3.eth.getTransactionCount(self.wallet.public_address)
-
-    def initContract(self):
         self.contract = self.web3.eth.contract(self.contract_address, abi=self.abi_json)
+
 
     def execute(self, context):
         print(f"Executing {self.function} with args {self.function_args} on {self.contract_address}")
         raw_txn = self.function(**self.function_args)\
                              .buildTransaction(dict(
                                nonce=int(self.nonce),
-                               gasPrice = int(max(33,self.web3.eth.gasPrice) *\
+                               gasPrice = int(self.web3.eth.gasPrice *\
                                           self.gas_multiplier),
                                gas = self.gas
                               ))
--- dags/blocksec_plugin/erc20_approve_operator.py
+++ dags/blocksec_plugin/erc20_approve_operator.py
@@ -1,7 +1,10 @@
 from airflow.utils.decorators import apply_defaults
 from blocksec_plugin.contract_interaction_operator import ContractInteractionOperator
-from blocksec_plugin.abis import ERC20_ABI
 
+
+ERC20_ABI = '''[{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},
+{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+'''
 
 class ERC20ApprovalOperator(ContractInteractionOperator):
 
@@ -19,7 +22,6 @@ class ERC20ApprovalOperator(ContractInteractionOperator):
         super().__init__(abi_json=ERC20_ABI, *args, **kwargs)
 
     def execute(self, context):
-        self.initContract()
         # Check if the approve should use the max balance
         if int(self.amount) < 0:
             self.amount = self.contract.functions.balanceOf(self.wallet.public_address).call()
