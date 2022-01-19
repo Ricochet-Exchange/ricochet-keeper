0e293ed97d942b939a4435b4c227fa5e60caa1e8
Michael Ghen <mike@mikeghen.com>
2022-01-19 08:00:36 -0500

    parameterize variables on v2 workflow

--- dags/ricochet_distribute_v2.py
+++ dags/ricochet_distribute_v2.py
@@ -20,10 +20,10 @@ from blocksec_plugin.ricochet_distributeV2_operator import RicochetDistributeOpe
 from blocksec_plugin.ricochet_update_price_operator import RicochetUpdatePriceOperator
 
 
-DISTRIBUTOR_WALLET_ADDRESS = Variable.get("distributor-address")
+DISTRIBUTOR_WALLET_ADDRESS = Variable.get("distributor-v2-address")
 V2_EXCHANGE_ADDRESSES = Variable.get("rexmarket-v2-addresses", deserialize_json=True)
-SCHEDULE_INTERVAL = Variable.get("distribution-schedule-interval", "0 * * * *")
-GAS_MULTIPLIER = int(Variable.get("distribution-gas-multiplier", 1.1))
+SCHEDULE_INTERVAL = Variable.get("distribution-v2-schedule-interval", "0 * * * *")
+GAS_MULTIPLIER = int(Variable.get("distribution-v2-gas-multiplier", 1.1))
 USDCX_ADDRESS = "0xCAa7349CEA390F89641fe306D93591f87595dc1F"
 RIC_ADDRESS = "0x263026E7e53DBFDce5ae55Ade22493f828922965"
 
