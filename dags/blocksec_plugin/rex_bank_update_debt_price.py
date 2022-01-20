7166e22a60eeae38b7b8bcd48bb0a8b69a173c81
Michael Ghen <mike@mikeghen.com>
2022-01-20 15:21:39 -0500

    fix typos

--- dags/blocksec_plugin/rex_bank_update_debt_price.py
+++ dags/blocksec_plugin/rex_bank_update_debt_price.py
@@ -3,12 +3,10 @@ from blocksec_plugin.abis import REX_BANK_ABI
 from blocksec_plugin.contract_interaction_operator import ContractInteractionOperator
 
 class RexBankUpdateDebtPriceOperator(ContractInteractionOperator):
-    template_fields = ['amount']
     ui_color = "#ADF5FF"
 
     @apply_defaults
     def __init__(self,
-                 amount,
                  *args,
                  **kwargs):
         super().__init__(abi_json=REX_BANK_ABI, *args, **kwargs)
