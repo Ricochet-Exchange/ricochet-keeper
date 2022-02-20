669ec63fdaf283ca48724712ab9b3acd4770dfbb
samirsalem <slmsr90@gmail.com>
2022-02-21 00:49:28 +0100

    working docker-compose with celery, pg, airflow, and fernet secrets

---
+++ scripts/create_db.sql
@@ -0,0 +1,6 @@
+CREATE USER airflow PASSWORD 'postgres_password';
+CREATE ROLE
+postgres=# CREATE DATABASE airflow;
+CREATE DATABASE
+postgres=# GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO newt;
+GRANT
