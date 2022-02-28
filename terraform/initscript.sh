b323ddb8473c033b8a20e38af69c36475622f268
Samir Ahmed Hassn Salem <slmsr90@gmail.com>
2022-02-28 14:12:57 +0100

    corrections

--- terraform/initscript.sh
+++ terraform/initscript.sh
@@ -7,7 +7,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings#docke
 apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -a -G docker ubuntu
 sudo -u ubuntu mkdir -p /home/ubuntu/.docker/cli-plugins#
-sudo -u ubuntu curl -SL https://github.com/docker/compose/releases#download/v2.2.3/docker-compose-linux-x86_64 -o /home/ubuntu/.docker/cli-plugins#docker-compose
+sudo -u ubuntu curl -SL https://github.com/docker/compose/releases#download/v2.2.3/docker-compose-linux-x86_64 -o /home/ubuntu/.docker/cli-plugins/docker-compose
 chmod +x /home/ubuntu/.docker/cli-plugins#docker-compose
 ln -s /home/ubuntu/.docker/cli-plugins#docker-compose /usr/bin/docker-compose
 
@@ -18,7 +18,7 @@ echo -e '/[sshd]/afilter = sshd\nlogpath = /var/log/auth.log\nmaxretry = 3\nbant
 echo "clone project and variables substitution"
 sudo -u ubuntu git clone --branch ${keeper_repository_branch} ${keeper_repository} /home/ubuntu/ricochet-keeper
 cd /home/ubuntu/ricochet-keeper
-sed -e "s#key1#${key1}#g;s#address1#${address1}#g;s#key2#${key2}#g;s#address2#${address2}#g;s#key3#${key3}#g;s#address3#${address3}#g;s#key4#${key4}#g;s#address4#${address4}#g;s#key5#${key5}#g;s#address5#${address5}#g;s#key6#${key6}#g;s#address6#${address6}#g;s#key7#${key7}#g;s#address7#${address7}#g;s#gateway-URI#${gateway_uri}#g;s#gateway-WSS#${gateway_wss}#g;s#airflow_password#${airflow_password}#g;s#postgres_password#${postgres_password}#g" .vars
+sed -i -e "s#key1#${key1}#g;s#address1#${address1}#g;s#key2#${key2}#g;s#address2#${address2}#g;s#key3#${key3}#g;s#address3#${address3}#g;s#key4#${key4}#g;s#address4#${address4}#g;s#key5#${key5}#g;s#address5#${address5}#g;s#key6#${key6}#g;s#address6#${address6}#g;s#key7#${key7}#g;s#address7#${address7}#g;s#gateway-URI#${gateway_uri}#g;s#gateway-WSS#${gateway_wss}#g;s#airflow_password#${airflow_password}#g;s#postgres_password#${postgres_password}#g" .vars
 
 
 echo "Prepare database and run keeper"
