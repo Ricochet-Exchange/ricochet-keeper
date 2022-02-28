84825366573af7b04a67cb4ad880d59f7b032d74
Samir Ahmed Hassn Salem <slmsr90@gmail.com>
2022-02-28 02:20:39 +0100

    just another sed syntax test

--- terraform/initscript.sh
+++ terraform/initscript.sh
@@ -16,7 +16,7 @@ ln -s /home/ubuntu/.docker/cli-plugins#docker-compose /usr/bin/docker-compose
 echo "clone project and variables substitution"
 sudo -u ubuntu git clone --branch ${keeper_repository_branch} ${keeper_repository} /home/ubuntu/ricochet-keeper
 cd /home/ubuntu/ricochet-keeper
-sed -e 's#key1#${key1};s#address1#${address1};s#key2#${key2};s#address2#${address2};s#key3#${key3};s#address3#${address3};s#key4#${key4};s#address4#${address4};s#key5#${key5};s#address5#${address5};s#key6#${key6};s#address6#${address6};s#key7#${key7};s#address7#${address7};s#gateway-URI#${gateway_uri};s#gateway-WSS#${gateway_wss}#i;s#airflow_password#${airflow_password};s#postgres_password#${postgres_password}' .vars
+sed -e "s#key1#${key1};s#address1#${address1};s#key2#${key2};s#address2#${address2};s#key3#${key3};s#address3#${address3};s#key4#${key4};s#address4#${address4};s#key5#${key5};s#address5#${address5};s#key6#${key6};s#address6#${address6};s#key7#${key7};s#address7#${address7};s#gateway-URI#${gateway_uri};s#gateway-WSS#${gateway_wss}#i;s#airflow_password#${airflow_password};s#postgres_password#${postgres_password}" .vars
 
 
 echo "Prepare database and run keeper"
