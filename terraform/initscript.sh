baded54cd2d4656e0d99390d3f0ba1b8cdba0bf5
Samir Ahmed Hassn Salem <slmsr90@gmail.com>
2022-02-28 16:28:44 +0100

    corrections

--- terraform/initscript.sh
+++ terraform/initscript.sh
@@ -3,7 +3,7 @@ set -xe
 echo "Prepare install"
 apt-get install -y ca-certificates curl gnupg lsb-release sudo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings#docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -a -G docker ubuntu
 sudo -u ubuntu DOCKER_CONFIG=/home/ubuntu/.docker
