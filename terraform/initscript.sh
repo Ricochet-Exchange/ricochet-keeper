c73fc51b6d0680f524d8cef6ed0642dee31ba75d
Vincent Lacointe <vlacointe@gmail.com>
2022-02-28 12:15:57 +0100

    fix sed command and var file

--- terraform/initscript.sh
+++ terraform/initscript.sh
@@ -11,12 +11,14 @@ sudo -u ubuntu curl -SL https://github.com/docker/compose/releases#download/v2.2
 chmod +x /home/ubuntu/.docker/cli-plugins#docker-compose
 ln -s /home/ubuntu/.docker/cli-plugins#docker-compose /usr/bin/docker-compose
 
-
+echo "Fail2ban deployment"
+apt-get install -y fail2ban
+echo -e '/[sshd]/afilter = sshd\nlogpath = /var/log/auth.log\nmaxretry = 3\nbantime = 1h' /etc/fail2ban/jail.d/defaults-debian.conf
 
 echo "clone project and variables substitution"
 sudo -u ubuntu git clone --branch ${keeper_repository_branch} ${keeper_repository} /home/ubuntu/ricochet-keeper
 cd /home/ubuntu/ricochet-keeper
-sed -e "s#key1#${key1};s#address1#${address1};s#key2#${key2};s#address2#${address2};s#key3#${key3};s#address3#${address3};s#key4#${key4};s#address4#${address4};s#key5#${key5};s#address5#${address5};s#key6#${key6};s#address6#${address6};s#key7#${key7};s#address7#${address7};s#gateway-URI#${gateway_uri};s#gateway-WSS#${gateway_wss}#;s#airflow_password#${airflow_password};s#postgres_password#${postgres_password}" .vars
+sed -e "s#key1#${key1}#g;s#address1#${address1}#g;s#key2#${key2}#g;s#address2#${address2}#g;s#key3#${key3}#g;s#address3#${address3}#g;s#key4#${key4}#g;s#address4#${address4}#g;s#key5#${key5}#g;s#address5#${address5}#g;s#key6#${key6}#g;s#address6#${address6}#g;s#key7#${key7}#g;s#address7#${address7}#g;s#gateway-URI#${gateway_uri}#g;s#gateway-WSS#${gateway_wss}#g;s#airflow_password#${airflow_password}#g;s#postgres_password#${postgres_password}#g" .vars
 
 
 echo "Prepare database and run keeper"
