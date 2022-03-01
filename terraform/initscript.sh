9f118e772e867186072f090173261c902efe158e
Samir Ahmed Hassn Salem <slmsr90@gmail.com>
2022-03-01 19:54:46 +0100

    avoid using && with make.sh for ease of debug

--- terraform/initscript.sh
+++ terraform/initscript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -xe
+DIRECTORY=/home/ubuntu/ricochet-keeper
+DOCKER_CONFIG=/home/ubuntu/.docker
+
 echo "Prepare install"
 apt-get install -y ca-certificates curl gnupg lsb-release sudo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -a -G docker ubuntu
-export DOCKER_CONFIG=/home/ubuntu/.docker
 sudo -u ubuntu mkdir -p $DOCKER_CONFIG/cli-plugins
 sudo -u ubuntu curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x /home/ubuntu/.docker/cli-plugins/docker-compose
@@ -43,4 +45,4 @@ export gateway_wss=${gateway_wss}
 envsubst < .vars.tmpl > .vars && chown ubuntu:ubuntu .vars
 
 echo "Prepare database and run keeper"
-su -l ubuntu -c "cd /home/ubuntu/ricochet-keeper; ./make.sh setup && ./make.sh deploy"
+su -l ubuntu -c "cd /home/ubuntu/ricochet-keeper; ./make.sh setup; ./make.sh deploy"
