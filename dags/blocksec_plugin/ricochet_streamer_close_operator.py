4fd7991a439186b2b8b19df5a51c500dc4dac4c8
Shreyas Papinwar <spapinwar@gmail.com>
2022-04-29 18:32:12 +0100

    testing

--- dags/blocksec_plugin/ricochet_streamer_close_operator.py
+++ dags/blocksec_plugin/ricochet_streamer_close_operator.py
@@ -7,7 +7,7 @@ class RicochetStreamerCloseOperator(ContractInteractionOperator):
     """
     Closes a streamers stream using `closeStream`
     """
-    template_fields = ['streamer_address', 'contract_address']
+    template_fields = ['streamer_address', 'exchange_address', 'nonce']
     ui_color = "#ADF5FF"
 
     @apply_defaults
